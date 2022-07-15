95c46b701a836aba11e835c97bcbaf6f57f8c9ac
Chukwuma Obasi <69759467+ccobasi@users.noreply.github.com>
2022-07-15 20:35:41 +0100

    add models author and book

--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
+from tkinter import CASCADE
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    first_name = models.TextField()
+    last_name = models.TextField()
+
+    def __str__(self):
+        return self.first_name
+
+
+class Book(models.Model):
+    name = models.TextField()
+    isbn = models.TextField()
+    author = models.ForeignKey(
+        Author, related_name="authors", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
