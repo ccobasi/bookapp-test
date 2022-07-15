7b9e19b6b000e39dc6ad26463e828710f455816c
Chukwuma Obasi <69759467+ccobasi@users.noreply.github.com>
2022-07-15 19:29:50 +0100

    template setup

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,60 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav class="navbar is-dark">
+    <div class="nav-brand">
+      <router-link to="/" class="navbar-item"
+        ><strong>Waje Smart Solutions</strong></router-link
+      >
+
+      <a
+        class="navbar-burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbar-menu"
+        @click="showMobileMenu = !showMobileMenu"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div
+      class="navbar-menu"
+      id="navbar-menu"
+      v-bind:class="{ 'is-active': showMobileMenu }"
+    >
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <form method="get" action="/search">
+            <div class="field has-addons">
+              <div class="control">
+                <input
+                  type="text"
+                  class="input"
+                  placeholder="What are you looking for?"
+                  name="query"
+                />
+              </div>
+
+              <div class="control">
+                <button class="button is-success">
+                  <span class="icon">
+                    <i class="fas fa-search"></i>
+                  </span>
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   </nav>
-  <router-view/>
+  <div>
+    <router-view />
+  </div>
+  <router-view />
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import "../node_modules/bulma";
 </style>
