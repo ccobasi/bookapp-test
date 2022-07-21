82db92e85d6e13e6b824439a86f5f5d7b5aef129
Chukwuma Obasi <69759467+ccobasi@users.noreply.github.com>
2022-07-21 20:46:57 +0100

    vue initial commit

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,34 +1,37 @@
 <template>
+<div>
   <section class="hero is-medium is-info mb-6">
     <div class="hero-body has-text-centered">
       <p class="title mb-6">Book App</p>
       <p class="subtitle">The best book store online</p>
     </div>
   </section>
-  <div class="columns is-multiline">
-    <div class="column is-12">
-      <h2 class="is-size-2 has-text-centered">List of Books</h2>
-    </div>
 
-    <div
-      class="column is-3"
-      v-for="book in bookLists"
-      v-bind:key="book.id"
-      v-bind:book="book"
-    >
-      <div>
-        <h3 class="is-size-4">{{ book.name }}</h3>
-        <p class="is-size-6 has-text-grey">{{ book.author }}</p>
-
-        <router-view
-          v-bind:to="book.get_absolute_url"
-          class="button is-primary mt-4"
-        >
-          View Details
-        </router-view>
+<div class="columns is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered">List of Books</h2>
       </div>
-    </div>
+
+        <div
+          class="column is-3"
+          v-for="book in bookLists"
+          v-bind:key="book.id"
+          v-bind:book="book"
+        >
+          <div>
+            <h3 class="is-size-4">{{ book.name }}</h3>
+            <p class="is-size-6 has-text-grey">{{ book.author }}</p>
+
+            <router-view
+              v-bind:to="book.get_absolute_url"
+              class="button is-primary mt-4"
+            >
+              View Details
+            </router-view>
+          </div>
+      </div>
   </div>
+</div>
 </template>
 
 <script>
