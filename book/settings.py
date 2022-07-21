e5897b7f85ebb3f50e9ff402ace6cc3a904bfdb9
Chukwuma Obasi <69759467+ccobasi@users.noreply.github.com>
2022-07-21 15:48:55 +0100

    add django on heroku config

--- book/settings.py
+++ book/settings.py
@@ -131,4 +131,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-django_heroku.settings(locals())
+# Configure Django App for Heroku.
+import django_on_heroku
+django_on_heroku.settings(locals())
